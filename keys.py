19d3fa5b3b5d7ff3475d36b1f46162efe5728ec2
Ahmed Shahriar <47810596+shahriar0247@users.noreply.github.com>
2020-09-26 16:28:30 -0400

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,18 +1,68 @@
 import keyboard
 import time
 
-def write (key):
-    #time.sleep(1 / 10)
-    keyboard.write(key)
 
-keyboard.add_hotkey('ctrl+alt+1', lambda: write("5"))
-keyboard.add_hotkey('ctrl+alt+2', lambda: write("6"))
-keyboard.add_hotkey('ctrl+alt+9', lambda: write("-"))
-keyboard.add_hotkey('ctrl+alt+0', lambda: write("+"))
+trigger = "True"
+with open("trigger", "w") as f:
+        f.write(str(trigger))
 
-keyboard.add_hotkey('ctrl+alt+shift+1', lambda: write("%"))
-keyboard.add_hotkey('ctrl+alt+shift+2', lambda: write("^"))
-keyboard.add_hotkey('ctrl+alt+shift+9', lambda: write("_"))
-keyboard.add_hotkey('ctrl+alt+shift+0', lambda: write("="))
 
-keyboard.wait('ctrl+alt+1+3')
+def settrigger():
+    time.sleep(1 / 10)
+    global trigger
+    if trigger == "False": 
+        trigger = "True"
+    else:
+        trigger = "False"
+    with open("trigger", "w") as f:
+        f.write(str(trigger))
+    print("trigger set")
+
+
+keyboard.add_hotkey('shift+caps_lock', lambda: settrigger())
+
+
+from pynput.keyboard import Key, Controller, Listener
+
+keyboard2 = Controller()
+
+def key_type(keyboard2, keytotype):
+    keyboard2.press(Key.backspace)
+    keyboard2.release(Key.backspace)
+    keyboard2.type(keytotype)
+
+def on_press(key):
+    with open("trigger", "r") as f:
+        trigger = f.readline()
+    print(key)
+    if trigger == "True":
+        try:
+            if key.char == "1":
+                key_type(keyboard2,"5")
+            elif key.char == "2":
+                key_type(keyboard2,"6")
+            elif key.char == "7":
+                key_type(keyboard2,"-")
+            elif key.char == "8":
+                key_type(keyboard2,"+")
+            elif key.char == "3":
+                key_type(keyboard2,"%")
+            elif key.char == "4":
+                key_type(keyboard2,"^")
+            elif key.char == "9":
+                key_type(keyboard2,"_")
+            elif key.char == "0":
+                key_type(keyboard2,"=")
+    
+              
+            
+        except:
+            pass
+
+def on_release(key):
+    pass
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
+keyboard.wait('space+1+3')
